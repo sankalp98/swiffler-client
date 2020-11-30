1eaede5dd1b39a5e78c2b6db3b8a70a96f383d80
sankalp kasale <sankalpkasale@sankalps-MacBook-Air.local>
2020-11-30 21:34:20 +0530

    fixed proxy and server url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/client'
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000/'
+    uri: 'https://lit-springs-76899.herokuapp.com/'
 })
 
 const authLink = setContext((request, previousContext) => {
